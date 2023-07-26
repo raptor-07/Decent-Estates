c0841a6e20d0cf4c1e812318dc4b3093bdfb0bde
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-07-26 20:27:29 +0530

    feat: add contract to mint NFT

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,6 +1,25 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract RealEstate {
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+
+contract RealEstate is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("RealEstate", "RE") {}
+
+    function mint(address _to, string memory _tokenURI)
+        public
+        returns (uint256)
+    {
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+        _mint(_to, newTokenId);
+        _setTokenURI(newTokenId, _tokenURI);
+        return newTokenId;
+    }
 }
